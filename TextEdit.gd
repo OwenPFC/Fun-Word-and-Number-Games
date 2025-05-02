f3ed930de34ec3a8dc80dac7b10095ab7234e5c6
Misosoup97 <161241247+Misosoup97@users.noreply.github.com>
2025-05-01 21:42:17 -0400

    game files

--- TextEdit.gd
+++ TextEdit.gd
@@ -1,5 +1,5 @@
 extends TextEdit
-
+var totalTime = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -7,6 +7,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	totalTime+=delta
+	$".".text = str(totalTime)
 		
 	
